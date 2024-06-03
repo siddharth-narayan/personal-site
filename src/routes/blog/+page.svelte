ff421b6689926017377bea25bffa6d4b7d58941f
Siddharth <siddharth.060605@gmail.com>
2024-06-03 00:39:52 -0400

    Added nix, footer, favicon, and started blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,6 @@
-<div>BLOG!</div>
+<div class="flex flex-col gap-8">
+    <h1 class="text-4xl border-b border-secondary p-4 w-full text-center">Blog Posts</h1>
+    <div class="w-[40em] self-center text-justify">
+        <a href="/blog/post-quantum-cryptography/">Post Quantum Cryptography</a>
+    </div>
+</div>
