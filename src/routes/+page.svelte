f7b87b28877071961eb877f0e4435d6e3d2646a7
Siddharth <siddharth.060605@gmail.com>
2024-07-11 19:38:08 -0400

    Everything fits now!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,20 @@
     <title>My Personal Website</title> 
 </svelte:head>
 
-<div class="flex flex-col gap-8">
-    <h1 class="text-4xl border-b border-secondary p-4 w-full text-center">Welcome to my website !</h1>
-    <div class="w-[40em] self-center text-justify">
-        <p>Hello! This website is my personal website, it will have whatever I want on here. This website 
-            is written with sveltekit and tailwindcss, and kept as simple as possible. I'm probably not going 
-            to post much to my blog, but I'll put whatever I think is cool on here. 
-            <br><br>
-            Right now while writing this, 
-            the only thing I'm planning on putting on the blog is my adventure into post quantum cryptography. 
-            Oh! By the way, this website is hosted on Nginx built with OpenSSL 3.0 with oqsprovider. 
-            That means that if you have it enabled in Chrome or Firefox (I think almost all major browsers support it), 
-            you are probably connected to this website in a way that future quantum computers will not be able to break 
-            (assuming that kyber isn't broken in the future somehow). All though there's no need for that on a website like
-            this one, it <i>is</i> pretty cool to know!
-            <br><br>
-            I'll have more details on that in the blog post, if I ever get around to writing it + finishing this website!
-            <br><br>
-            Other than that, the "Useful websites" part will just be links to websites I think are cool, like the pre-google internet.
-        </p>
-    </div>
-</div>
+<h1 class="text-4xl border-b border-secondary p-4 w-full text-center">Welcome to my website !</h1>
+    <p>Hello! This website is my personal website, it will have whatever I want on here. This website 
+        is written with sveltekit and tailwindcss, and kept as simple as possible. I'm probably not going 
+        to post much to my blog, but I'll put whatever I think is cool on here. 
+        <br><br>
+        Right now while writing this, 
+        the only thing I'm planning on putting on the blog is my adventure into post quantum cryptography. 
+        Oh! By the way, this website is hosted on Nginx built with OpenSSL 3.0 with oqsprovider. 
+        That means that if you have it enabled in Chrome or Firefox (I think almost all major browsers support it), 
+        you are probably connected to this website in a way that future quantum computers will not be able to break 
+        (assuming that kyber isn't broken in the future somehow). All though there's no need for that on a website like
+        this one, it <i>is</i> pretty cool to know!
+        <br><br>
+        I'll have more details on that in the blog post, if I ever get around to writing it + finishing this website!
+        <br><br>
+        Other than that, the "Useful websites" part will just be links to websites I think are cool, like the pre-google internet.
+    </p>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,19 +2,13 @@
     <title>Blog Posts</title> 
 </svelte:head>
 
-<div class="flex flex-col gap-8">
-    <h1 class="text-4xl border-b border-secondary p-4 w-full text-center">Blog Posts</h1>
-    <div class="w-[20em] self-center text-justify text-2xl">
-        <ul>
-            <li><a href="/blog/post-quantum-cryptography/">Post Quantum Cryptography</a></li>
-        </ul>
-        
-    </div>
-</div>
+<h1 class="text-4xl border-b border-secondary p-4 w-full text-center">Blog Posts</h1>
+<ul class="self-center">
+    <li><a href="/blog/post-quantum-cryptography/">Post Quantum Cryptography</a></li>
+</ul>
 
 <style>
-    li::marker {
-        content: '• ';
-        font-size: 1.2em;
+    ul {
+        list-style-type: disc;
     }
 </style>
