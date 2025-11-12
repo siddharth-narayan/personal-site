36b34e325ef8ea82ace85de472f10efdb614aba8
Siddharth <siddharth.060605@gmail.com>
2025-11-12 00:21:48 -0600

    Upgrade npm package versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		"port": 7000
 	}
